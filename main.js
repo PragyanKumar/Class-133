56283e14ee9a6adaa7c03301b5e5fb575aec0da6
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2022-01-23 19:34:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 img=""
 Status=""
 objects=[]
-function preload(){
-    img=loadImage("dog_cat.jpg")
-}
 
 function setup(){
-    canvas=createCanvas(640,420)
+    canvas=createCanvas(380,380)
     canvas.center() 
+    video=createCapture(VIDEO)
+    video.size(380,380)
+    video.hide()
     objectDetected=ml5.objectDetector("cocossd",modelLoaded)
     document.getElementById("status").innerHTML="Status:Detectng objects"
 }
@@ -15,7 +15,6 @@ function setup(){
 function modelLoaded(){
     console.log("Model is Loaded")
     Status=true
-    objectDetected.detect(img,gotResult)
 }
 
 function gotResult(error,results){
@@ -27,16 +26,23 @@ function gotResult(error,results){
 }
 
 function draw(){
-    image (img,0,0,640,420)
+    image (video,0,0,380,380)
 
     if(Status !="" ){
+
+        r=random(255)
+        g=random(255)
+        b=random(255)
+
+        objectDetected.detect(video,gotResult)
         for (let i = 0; i< objects.length; i++) {
         document.getElementById("status").innerHTML="status: objects detected"
-        fill("#FF0000")
+        document.getElementById("number_of_object").innerHTML="Number of objects= " + objects.length
+        fill(r,g,b)
         percent=floor( objects[i].confidence * 100 )
         text( objects[i].label + " " + percent + "%", objects[i].x + 15 , objects[i].y + 15 )
         noFill()
-        stroke("#FF0000")
+        stroke(r,g,b)
         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
             
         }
